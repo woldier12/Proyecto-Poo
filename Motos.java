128eec874d029c329cccf1255aa2af2ca5ae87c9
Josue Vera <86860303+woldier12@users.noreply.github.com>
2021-07-02 21:51:34 -0500

    Add files via upload

---
+++ Motos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.proyectopoo;
+
+/**
+ *
+ * @author Josue Vera
+ */
+public class Motos extends Vehiculo {
+
+    public Motos(String placa, String marca, String modelo, String motor, int año, double recorrido, String color, String combustible, double precio) {
+        super(placa, marca, modelo, motor, año, recorrido, color, combustible, precio);
+    }
+
+
+    }
+    
+  
